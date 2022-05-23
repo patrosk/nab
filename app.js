ac67a70f3da4a6ee5ac6230f79ce7a57740767ac
patrosk <naiade99@gmail.com>
2022-05-23 17:09:58 +0200

    added about page and styling

---
+++ app.js
@@ -0,0 +1,9 @@
+// HAMBURGER AND DROPDOWN MENU ANIMATION
+const menuButton = document.querySelector('.dropdown-button');
+const dropdown = document.querySelector('.dropdown-content');
+const hamburgerSpans = document.querySelector('.hamburger span');
+
+menuButton.addEventListener('click', () => {
+  dropdown.classList.toggle('show');
+  hamburgerSpans.classList.toggle('is-active');
+});
